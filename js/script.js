a17b2ebd5025ebaaf874d79c2d709385ead1f47b
Dinda Maharani <143940529+Dindamaharn@users.noreply.github.com>
2025-01-13 20:24:47 +0700

    Done Home Page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+// Slider JS Home Page
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    const slider = document.querySelector('.slider');
+    const offset = -index * 100; 
+    slider.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides; 
+    showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 3000); 
+
 
